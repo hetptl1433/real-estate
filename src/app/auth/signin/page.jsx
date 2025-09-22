9673bd0a3d788a2186fabf9adf5481694c678ea1
hetptl1433 <hetptl1433@gmail.com>
2025-09-22 15:05:38 -0500

    erro solving 4

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import SignInClient from "./SignInClient";
 
 export const dynamic = "force-dynamic"; // auth pages shouldn't be prerendered
-
+export const revalidate = 0;
 export default function Page() {
   return (
     <Suspense fallback={null}>
